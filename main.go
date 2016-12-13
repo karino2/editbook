a7cbb6ef1cfbcdb33fedd40d23a8d18c0af7870f
karino2 <hogeika2@gmailcom>
2016-12-14 05:23:11 +0900

    Initial commit. Basic open work.

---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/gorilla/websocket"
+)
+
+var cmdsch = make(chan string)
+
+func handleCommandConnection(conn net.Conn) {
+	messageWithLF, _ := bufio.NewReader(conn).ReadString('\n')
+	message := strings.TrimSpace(messageWithLF)
+	cmds := strings.SplitN(message, " ", 2)
+	cmd := cmds[0]
+	arg := cmds[1]
+	fmt.Println("cmd:", cmd, " arg:", arg)
+	cmdsch <- cmd
+	cmdsch <- arg
+}
+
+func saveFile(path string, body string) error {
+	bbody := []byte(body)
+	return ioutil.WriteFile(path, bbody, 0600)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	path := r.FormValue("path")
+	body := r.FormValue("body")
+	err := saveFile(path, body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/static/empty.thml", http.StatusFound)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	Subprotocols:    []string{"editbook"},
+}
+
+const (
+	clientOPEN = '0'
+)
+
+func sendData(conn *websocket.Conn, data []byte) error {
+	return conn.WriteMessage(websocket.TextMessage, data)
+}
+
+func openFileToClient(conn *websocket.Conn, path string) error {
+	tbody, _ := ioutil.ReadFile(path)
+
+	type OpenCmdJSON struct {
+		Path, Data string
+	}
+
+	jsondat, _ := json.Marshal(OpenCmdJSON{path, string(tbody)})
+
+	return sendData(conn, append([]byte{clientOPEN}, []byte(jsondat)...))
+}
+
+func wsSendReceive(cmdsch chan string, conn *websocket.Conn) {
+	log.Println("deb3")
+	exit := make(chan bool, 2)
+
+	// should handle disconnect someway.
+	go func() {
+		defer func() { exit <- true }()
+
+		cmd := <-cmdsch
+		arg := <-cmdsch
+		log.Println("deb4")
+		switch cmd {
+		case "open":
+			openFileToClient(conn, arg)
+		}
+	}()
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("wsHandler called")
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	log.Println("deb1")
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("Failed to upgrade connection: " + err.Error())
+		return
+	}
+	log.Println("deb2")
+
+	go wsSendReceive(cmdsch, conn)
+}
+
+func main() {
+	log.Println("start main")
+
+	fileServer := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/ws", wsHandler)
+	http.Handle("/static/", fileServer)
+	go http.ListenAndServe(":5123", nil)
+	ln, err := net.Listen("tcp", ":5124")
+	if err != nil {
+		fmt.Printf("Can't open command socket %s\n", err)
+		os.Exit(1)
+	}
+	for {
+		conn, _ := ln.Accept()
+		go handleCommandConnection(conn)
+
+	}
+
+}
